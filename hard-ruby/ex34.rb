655a085b420e00bd66d4c0e1d9dd4bc848ae8a65
leapon <uestudio@gmail.com>
2015-06-25 11:30:54 +0800

    more ex for ruby

---
+++ hard-ruby/ex34.rb
@@ -0,0 +1,7 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+puts "animals: #{animals}"
+
+puts "first:" + animals[0]
+
+puts "last:" + animals[animals.length - 1]
+
---
+++ hard-ruby/mystuffclass.rb
@@ -0,0 +1,14 @@
+class MyStuffClass
+
+    def initialize()
+        @tangerine = "And now a thousand years between"
+    end
+
+    attr_reader :tangerine
+
+    def apple()
+        puts "I AM CLASSY APPLES!"
+    end
+
+end
+
