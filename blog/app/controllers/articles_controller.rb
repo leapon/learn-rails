704b32ea88683db097f70f2e08089256a56e24c2
leapon <uestudio@gmail.com>
2015-06-26 14:31:09 +0800

    article show and list pages updated

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
   end
   
   def new
@@ -24,7 +25,11 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
-   
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :text)
