5798ee5d98b18723470dc7f6c257b808740b023b
leapon <uestudio@gmail.com>
2015-06-26 14:21:17 +0800

    article model added; article save handled in create

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,3 +1,15 @@
+#articles routes
+#
+#       Prefix Verb   URI Pattern                  Controller#Action
+#     articles GET    /articles(.:format)          articles#index
+#              POST   /articles(.:format)          articles#create
+#  new_article GET    /articles/new(.:format)      articles#new
+# edit_article GET    /articles/:id/edit(.:format) articles#edit
+#      article GET    /articles/:id(.:format)      articles#show
+#              PATCH  /articles/:id(.:format)      articles#update
+#              PUT    /articles/:id(.:format)      articles#update
+#              DELETE /articles/:id(.:format)      articles#destroy
+
 class ArticlesController < ApplicationController
 
   def index
@@ -5,5 +17,17 @@ class ArticlesController < ApplicationController
   
   def new
   end
+  
+  def create
+    @article = Article.new(article_params)
+   
+    @article.save
+    redirect_to @article
+  end
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
   
 end
