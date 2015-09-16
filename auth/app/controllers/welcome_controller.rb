8fda1702ade5c7cb1a76b8b0fe569d90be623bc2
leapon <leapon@gmail.com>
2015-09-16 16:07:00 -0400

    params is obtained in callback

--- auth/app/controllers/welcome_controller.rb
+++ auth/app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
   # Called to receive the token from Google Authentication
   def google_apps_token
     
-    #puts '>>> callback from google apps:', omni_params
+    puts '>>> callback from google apps params:', params
     #puts '>>> receive_token parameters:', omni_params['user_id'], omni_auth['credentials']
     
     render :text => 'google apps token'
