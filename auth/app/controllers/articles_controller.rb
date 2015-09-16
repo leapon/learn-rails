b9c360d15354b570ce6f57ce122c04ef92ca3c42
leapon <leapon@gmail.com>
2015-09-16 11:19:22 -0400

    auth project added for testing google oauth2

---
+++ auth/app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+#articles routes
+#
+#       Prefix Verb   URI Pattern                  Controller#Action
+#     articles GET    /articles(.:format)          articles#index
+#              POST   /articles(.:format)          articles#create
+#  new_article GET    /articles/new(.:format)      articles#new
+# edit_article GET    /articles/:id/edit(.:format) articles#edit
+#      article GET    /articles/:id(.:format)      articles#show
+#              PATCH  /articles/:id(.:format)      articles#update
+#              PUT    /articles/:id(.:format)      articles#update
+#              DELETE /articles/:id(.:format)      articles#destroy
+
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+   
+    @article.save
+    redirect_to @article
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+  
+end
