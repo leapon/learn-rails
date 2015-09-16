91fff33cdcd2af7703ac6d32cbed24032a6aacca
leapon <leapon@gmail.com>
2015-09-16 16:46:54 -0400

    output request.env["omniauth.auth"]

--- auth/app/controllers/welcome_controller.rb
+++ auth/app/controllers/welcome_controller.rb
@@ -6,6 +6,8 @@ class WelcomeController < ApplicationController
   # Called to receive the token from Google Authentication
   def google_apps_token
     
+    puts '>>> request.env["omniauth.auth"]:', request.env["omniauth.auth"]
+    
     puts '>>> callback from google apps params:', params
     #puts '>>> receive_token parameters:', omni_params['user_id'], omni_auth['credentials']
     
