61b0a205bfea4c029f904f16979dfba0be69e420
leapon <leapon@gmail.com>
2015-09-16 15:26:38 -0400

    add route and method for /auth/google_oauth2/callback

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/auth/google_oauth2/callback' => 'welcome#google_apps_token'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
