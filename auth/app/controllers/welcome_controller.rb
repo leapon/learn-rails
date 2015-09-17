369b75b9ca8c5f5d61e0ac54c3713cf3199e4f3f
leapon <leapon@gmail.com>
2015-09-17 10:00:57 -0400

    output omni_params content

--- auth/app/controllers/welcome_controller.rb
+++ auth/app/controllers/welcome_controller.rb
@@ -7,23 +7,25 @@ class WelcomeController < ApplicationController
   def google_apps_token
     
     puts '>>> request.env["omniauth.auth"]:', request.env["omniauth.auth"]
-    
     puts '>>> callback from google apps params:', params
-    #puts '>>> receive_token parameters:', omni_params['user_id'], omni_auth['credentials']
+    
+    omni_params = request.env["omniauth.auth"]
+    puts ">>> omni_params:", omni_params.to_s
+    
+    puts ">>> omni_params.uid:", omni_params.uid
+    puts ">>> omni_params.provider:", omni_params.provider
+    puts ">>> omni_params.info:", omni_params.info
+    puts ">>> omni_params.credentials:", omni_params.credentials
+    puts ">>> omni_params.extra:", omni_params.extra
     
     render :text => 'google apps token'
     
   end
   
   def google_apps_token2
-    
-    puts '>>> request.env["omniauth.auth"]:', request.env["omniauth.auth"]
-    
-    puts '>>> callback from google apps params:', params
-    #puts '>>> receive_token parameters:', omni_params['user_id'], omni_auth['credentials']
-    
+    #puts '>>> request.env["omniauth.auth"]:', request.env["omniauth.auth"]
+    #puts '>>> callback from google apps params:', params
     render :text => 'google apps token2'
-    
   end
   
 end
